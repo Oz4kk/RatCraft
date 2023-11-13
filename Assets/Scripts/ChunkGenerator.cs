b71d3ffee62f151281e9a7befb397530359f5845
Oz4kk <120325589+Oz4kk@users.noreply.github.com>
2023-11-13 17:52:00 +0100

    MapGenerator and ChunkGenerator scripts separated

---
+++ Assets/Scripts/ChunkGenerator.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGenerator : MonoBehaviour
+{
+    [Serializable]
+    private struct GridSize
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public GridSize(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    private MapGenerator mapGenerator;
+
+    [SerializeField] private GridSize gridSize = new GridSize(5, 5, 5);
+    [SerializeField] private float sidesPerlinScale = 0.0f;
+    [SerializeField] private float yPerlinScale = 0.1f;
+    [SerializeField] private float heightLimit = 0.0f;
+
+    private void Awake()
+    {
+        mapGenerator = GetComponent<MapGenerator>();
+    }
+
+    public void GenerateChunk()
+    {
+        uint debugBlueCubeCounter = 0;
+        uint dubugGreenCubeCounter = 0;
+        uint debugBrownCubeCounter = 0;
+        uint debugPinkCubeCounter = 0;
+        float[] debugSample = new float[gridSize.x * gridSize.y * gridSize.z];
+        int debugSampleCounter = 0;
+
+
+        for (float x = 0; x < gridSize.x; x++)
+        {
+            for (float y = 0; y < gridSize.y; y++)
+            {
+                for (float z = 0; z < gridSize.z; z++)
+                {
+                    float perlinValueCubes = Mathf.PerlinNoise(x * sidesPerlinScale + mapGenerator.seed, z * sidesPerlinScale + mapGenerator.seed);
+
+                    if (perlinValueCubes > heightLimit * y)
+                    {
+                        float sample = Mathf.PerlinNoise(Mathf.Floor(x / 5) * sidesPerlinScale + mapGenerator.seed, Mathf.Floor(z / 5) * sidesPerlinScale + mapGenerator.seed);
+                        float sampleY = Mathf.PerlinNoise(Mathf.Floor(y / 2) * yPerlinScale + mapGenerator.seed, Mathf.Floor(y / 2) * yPerlinScale + mapGenerator.seed);
+
+                        debugSample[debugSampleCounter++] = sample + sampleY;
+
+                        Vector3 upcomingCubePosition = new Vector3(x, y, z);
+
+                        if (sample + sampleY > 0.875)
+                        {
+                            mapGenerator.InstantiateCube(upcomingCubePosition, mapGenerator.greenCube);
+                            dubugGreenCubeCounter++;
+                        }
+                        else if (sample + sampleY > 0.75)
+                        {
+                            mapGenerator.InstantiateCube(upcomingCubePosition, mapGenerator.blueCube);
+                            debugBlueCubeCounter++;
+                        }
+                        else if (sample + sampleY > 0.675)
+                        {
+                            mapGenerator.InstantiateCube(upcomingCubePosition, mapGenerator.brownCube);
+                            debugBrownCubeCounter++;
+                        }
+                        else
+                        {
+                            mapGenerator.InstantiateCube(upcomingCubePosition, mapGenerator.pinkCube);
+                            debugPinkCubeCounter++;
+                        }
+                    }
+                }
+            }
+        }
+        DebugManager.Log($"DebugSample Max - {Mathf.Max(debugSample)}");
+        DebugManager.Log($"DebugSample Min - {Mathf.Min(debugSample)}");
+        DebugManager.Log($"Count of green cubes - {dubugGreenCubeCounter}");
+        DebugManager.Log($"Count of blue cubes - {debugBlueCubeCounter}");
+        DebugManager.Log($"Count of brown cubes - {debugBrownCubeCounter}");
+        DebugManager.Log($"Count of pink cubes - {debugPinkCubeCounter}");
+    }
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,96 +5,22 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [Serializable]
-    private struct GridSize
-    {
-        public int x;
-        public int y;
-        public int z;
+    public GameObject greenCube;
+    public GameObject blueCube;
+    public GameObject brownCube;
+    public GameObject pinkCube;
 
-        public GridSize(int x, int y, int z)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-    }
-
-    [SerializeField] public GameObject greenCube;
-    [SerializeField] public GameObject blueCube;
-    [SerializeField] public GameObject brownCube;
-    [SerializeField] public GameObject pinkCube;
-    [SerializeField] private GridSize gridSize = new GridSize(5, 5, 5);
-
-    [SerializeField] private float sidesPerlinScale = 0.0f;    
-    [SerializeField] private float yPerlinScale = 0.1f;
-    [SerializeField] private float heightLimit = 0.0f;
-    [SerializeField] private float seed;
+    private ChunkGenerator chunkGenerator;
 
     public Dictionary<Vector3, CubeParameters> mapField = new Dictionary<Vector3, CubeParameters>();
+    public float seed;
+
 
     private void Start()
     {
-        GenerateMap();
-    }
+        chunkGenerator = GetComponent<ChunkGenerator>();
 
-    private void GenerateMap()
-    {
-        uint debugBlueCubeCounter = 0;
-        uint dubugGreenCubeCounter = 0;
-        uint debugBrownCubeCounter = 0;
-        uint debugPinkCubeCounter = 0;
-        float[] debugSample = new float[gridSize.x * gridSize.y * gridSize.z];
-        int debugSampleCounter = 0;
-
-
-        for (float x = 0; x < gridSize.x; x++)
-        {
-            for (float y = 0; y < gridSize.y; y++)
-            {
-                for (float z = 0; z < gridSize.z; z++)
-                {
-                    float perlinValueCubes = Mathf.PerlinNoise(x * sidesPerlinScale + seed, z * sidesPerlinScale + seed);
-                    
-                    if (perlinValueCubes > heightLimit * y)
-                    {
-                        float sample = Mathf.PerlinNoise(Mathf.Floor(x/5) * sidesPerlinScale + seed, Mathf.Floor(z/5) * sidesPerlinScale + seed);
-                        float sampleY = Mathf.PerlinNoise(Mathf.Floor(y/2) * yPerlinScale + seed, Mathf.Floor(y/2) * yPerlinScale + seed);
-
-                        debugSample[debugSampleCounter++] = sample + sampleY;
-
-                        Vector3 upcomingCubePosition = new Vector3(x, y, z);
-
-                        if (sample + sampleY > 0.875)
-                        {
-                            InstantiateCube(upcomingCubePosition, greenCube);
-                            dubugGreenCubeCounter++;
-                        }
-                        else if (sample + sampleY > 0.75)
-                        {
-                            InstantiateCube(upcomingCubePosition, blueCube);
-                            debugBlueCubeCounter++;
-                        }
-                        else if (sample + sampleY > 0.675)
-                        {
-                            InstantiateCube(upcomingCubePosition, brownCube);
-                            debugBrownCubeCounter++;
-                        }
-                        else
-                        {
-                            InstantiateCube(upcomingCubePosition, pinkCube);
-                            debugPinkCubeCounter++;
-                        }
-                    }
-                }
-            }
-        }
-        DebugManager.Log($"DebugSample Max - {Mathf.Max(debugSample)}");
-        DebugManager.Log($"DebugSample Min - {Mathf.Min(debugSample)}");
-        DebugManager.Log($"Count of green cubes - {dubugGreenCubeCounter}");
-        DebugManager.Log($"Count of blue cubes - {debugBlueCubeCounter}");
-        DebugManager.Log($"Count of brown cubes - {debugBrownCubeCounter}");
-        DebugManager.Log($"Count of pink cubes - {debugPinkCubeCounter}");
+        chunkGenerator.GenerateChunk();
     }
 
     public void InstantiateCube(Vector3 spawnPosition, GameObject cubePrefab)
