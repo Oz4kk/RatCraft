8bd9de077b583d17f1330c5020b6e38146494fc5
Oz4kk <120325589+Oz4kk@users.noreply.github.com>
2024-01-22 15:38:54 +0100

    ChunkGenerator code clean

--- Assets/Scripts/Map/ChunkGenerator.cs
+++ Assets/Scripts/Map/ChunkGenerator.cs
@@ -8,16 +8,18 @@ public class ChunkGenerator : MonoBehaviour
     public Action<Dictionary<Vector3, GameObject>> onChunkGenerated;
 
     private MapGenerator mapGenerator;
-    private MapOptimalisation mapOptimalisation;
 
     [SerializeField] private float sidesPerlinScale = 0.0f;
     [SerializeField] private float heightLimit = 0.0f;
     [SerializeField] private float heightPerlinScale = 0.0f;
 
+    private float grassValue = 10.0f;
+    private float dirtValue = 7.0f;
+    private float rockValue = 2.0f;
+
     private void Awake()
     {
         mapGenerator = GetComponent<MapGenerator>();
-        mapOptimalisation = GetComponent<MapOptimalisation>();
     }
 
     public IEnumerator GenerateChunkCoroutine(Vector3 centerOfActualChunk)
@@ -26,12 +28,6 @@ public class ChunkGenerator : MonoBehaviour
         uint dubugGreenCubeCounter = 0;
         uint debugBrownCubeCounter = 0;
         uint debugPinkCubeCounter = 0;
-        float[] debugSample = new float[mapGenerator.gridSize.x * mapGenerator.gridSize.y * mapGenerator.gridSize.z];
-
-        List<bool> countOfCubesInChunk = new List<bool>();
-        bool[,,] doesBlockExistOnUpcomingCoordinate = new bool[100, 16, 100];
-
-        int debugSampleCounter = 0;
 
         int xCycleCounter = (int)centerOfActualChunk.x;
 
@@ -47,21 +43,8 @@ public class ChunkGenerator : MonoBehaviour
 
                     if (perlinValueCubes > heightLimit * y)
                     {
-
                         float sampleXZ = Mathf.PerlinNoise(Mathf.Floor(x / 5) * sidesPerlinScale + mapGenerator.seed, Mathf.Floor(z / 5) * sidesPerlinScale + mapGenerator.seed);
                         float sampleY = Mathf.PerlinNoise(Mathf.Floor(y / 2) * heightPerlinScale + mapGenerator.seed, Mathf.Floor(x / 2) * heightPerlinScale + mapGenerator.seed);
-
-                        //0.43423
-                        //0.5343
-
-                        debugSample[debugSampleCounter++] = (sampleXZ + sampleY) / 2;
-
-                        //debugSample[debugSampleCounter++] = sampleY;
-                        //if (sampleY != debug2)
-                        //{
-                        //    Debug.Log(sampleY);
-                        //    debug2 = sampleY;
-                        //}
 
                         Vector3 upcomingCubePosition = new Vector3(x, y, z);
 
@@ -93,39 +76,34 @@ public class ChunkGenerator : MonoBehaviour
                 }
             }
         }
-        DebugManager.Log($"DebugSample Max - {Mathf.Max(debugSample)}");
-        DebugManager.Log($"DebugSample Min - {Mathf.Min(debugSample)}");
         DebugManager.Log($"Count of green cubes - {dubugGreenCubeCounter}");
         DebugManager.Log($"Count of blue cubes - {debugBlueCubeCounter}");
         DebugManager.Log($"Count of brown cubes - {debugBrownCubeCounter}");
         DebugManager.Log($"Count of pink cubes - {debugPinkCubeCounter}");
 
-        // Check if neighbour chunk exist and take cubes that are neighbours with actual chunk and add it to that dictionary
-
         onChunkGenerated?.Invoke(actualChunkField);
     }
 
-    private void ChunkGenerationSequence(Vector3 upcomingCubePosition, GameObject actualCubecColor, ref uint actualCubeCounter, ref Dictionary<Vector3, GameObject> onChunkGenerated)
+    private void ChunkGenerationSequence(Vector3 upcomingCubePosition, GameObject actualCubecColor, ref uint debugActualCubeCounter, ref Dictionary<Vector3, GameObject> onChunkGenerated)
     {
         GameObject actualCube = mapGenerator.InstantiateAndReturnCube(upcomingCubePosition, actualCubecColor);
         onChunkGenerated.Add(actualCube.transform.position, actualCube);
-        actualCubeCounter++;
+        debugActualCubeCounter++;
         ChooseTexture(actualCube);
     }
 
     private void ChooseTexture(GameObject actualCube)
     {
         Material actualMaterial = actualCube.GetComponent<Renderer>().material;
-        //UGLY - Set variables for constants of if's
-        if (actualCube.transform.position.y > 10.0f)
+        if (actualCube.transform.position.y > grassValue)
         {
             actualMaterial.mainTexture = mapGenerator.grass;
         }
-        else if (actualCube.transform.position.y > 7.0f)
+        else if (actualCube.transform.position.y > dirtValue)
         {
             actualMaterial.mainTexture = mapGenerator.dirt;
         }
-        else if (actualCube.transform.position.y > 2.0f)
+        else if (actualCube.transform.position.y > rockValue)
         {
             actualMaterial.mainTexture = mapGenerator.rock;
         }
