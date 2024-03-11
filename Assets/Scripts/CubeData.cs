65fc334e465fd23e79fecbccfcc7ee9deccae72c
Oz4kk <120325589+Oz4kk@users.noreply.github.com>
2024-03-11 17:06:41 +0100

    Changed struct to class

--- Assets/Scripts/CubeData.cs
+++ Assets/Scripts/CubeData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct CubeData
+public class CubeData
 {
     public GameObject cubePrefab;
     public Vector3 position;
